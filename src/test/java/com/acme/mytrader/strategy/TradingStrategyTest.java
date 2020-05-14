2186aa44ce71ebb5239a380ba51a727b78e6afcf
iPopcorn <taypetrillo@gmail.com>
2020-05-14 08:10:44 +0100

    Implements ExecutionService. Adds a model class, Trade to hold information about trades. Adds tests for ExecutionServiceImpl.

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,6 +1,12 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionServiceImpl;
+import com.acme.mytrader.model.Trade;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class TradingStrategyTest {
     @Test
@@ -8,5 +14,31 @@ public class TradingStrategyTest {
         TradingStrategy strategy = new TradingStrategy();
 
         strategy.executeTradingStrategy();
+    }
+
+    @Test
+    public void testExecutionServiceBuy() {
+        ExecutionServiceImpl trader = new ExecutionServiceImpl();
+        Trade expectedTrade = new Trade("IBM", Trade.BUY, 55.0, 100);
+
+        trader.buy("IBM", 55.0, 100);
+
+        ArrayList<Trade> trades = trader.getTrades();
+
+        assertEquals(1, trades.size());
+        assertEquals(expectedTrade, trades.get(0));
+    }
+
+    @Test
+    public void testExecutionServiceSell() {
+        ExecutionServiceImpl trader = new ExecutionServiceImpl();
+        Trade expectedTrade = new Trade("IBM", Trade.SELL, 55.0, 100);
+
+        trader.sell("IBM", 55.0, 100);
+
+        ArrayList<Trade> trades = trader.getTrades();
+
+        assertEquals(1, trades.size());
+        assertEquals(expectedTrade, trades.get(0));
     }
 }
